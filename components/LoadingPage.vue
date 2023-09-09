fc8540bc6214b6bc7f85d664240c091b2f537bdc
Mahmud Hasan Rafid <mahmudrafid02@gmail.com>
2023-09-09 10:48:50 +0600

    feat: Changed loading animation from image to css for good performance

--- components/LoadingPage.vue
+++ components/LoadingPage.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="h-screen w-full flex items-center justify-center">
-    <h2 class="animate">
-      <img src="/afsakar-logo.png" class="w-40">
-    </h2>
+    <span class="loader" />
   </div>
 </template>
 
@@ -24,8 +22,8 @@
     }
 }
 
-.animate {
-    display: flex;
+.loader {
+  display: flex;
     justify-content: center;
     align-items: center;
     height: 100%;
@@ -36,5 +34,75 @@
     animation: load 1.2s infinite 0s ease-in-out;
     animation-direction: alternate;
     text-shadow: 0 0 1px white;
-}
+        transform: rotateZ(45deg);
+        perspective: 1000px;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        color: var(--vegas-gold);
+      }
+        .loader:before,
+        .loader:after {
+          content: '';
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: inherit;
+          height: inherit;
+          border-radius: 50%;
+          transform: rotateX(70deg);
+          animation: 1s spin linear infinite;
+        }
+        .loader:after {
+          color: var(--vegas-gold);
+          transform: rotateY(70deg);
+          animation-delay: .4s;
+        }
+
+      @keyframes rotate {
+        0% {
+          transform: translate(-50%, -50%) rotateZ(0deg);
+        }
+        100% {
+          transform: translate(-50%, -50%) rotateZ(360deg);
+        }
+      }
+
+      @keyframes rotateccw {
+        0% {
+          transform: translate(-50%, -50%) rotate(0deg);
+        }
+        100% {
+          transform: translate(-50%, -50%) rotate(-360deg);
+        }
+      }
+
+      @keyframes spin {
+        0%,
+        100% {
+          box-shadow: .2em 0px 0 0px currentcolor;
+        }
+        12% {
+          box-shadow: .2em .2em 0 0 currentcolor;
+        }
+        25% {
+          box-shadow: 0 .2em 0 0px currentcolor;
+        }
+        37% {
+          box-shadow: -.2em .2em 0 0 currentcolor;
+        }
+        50% {
+          box-shadow: -.2em 0 0 0 currentcolor;
+        }
+        62% {
+          box-shadow: -.2em -.2em 0 0 currentcolor;
+        }
+        75% {
+          box-shadow: 0px -.2em 0 0 currentcolor;
+        }
+        87% {
+          box-shadow: .2em -.2em 0 0 currentcolor;
+        }
+      }
 </style>
