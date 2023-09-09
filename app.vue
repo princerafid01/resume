a4dd7a40dd7d436db4fd76fbaed1d2a479bc26e1
Mahmud Hasan Rafid <mahmudrafid02@gmail.com>
2023-09-09 13:10:26 +0600

    fix: Changed Og attributes for Better Seo performance

--- app.vue
+++ app.vue
@@ -21,12 +21,24 @@ onMounted(() => {
 useHead({
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   charset: 'utf-8',
-  meta: [{ name: 'description', content: 'Mahmud Rafid - Backend Engineer - Resume and Portfolio' }],
-  ogTitle: 'Mahmud Rafid - Backend Engineer - Resume and Portfolio',
-  description: 'Welcome to Mahmud Rafid\'s resume and portfolio site. Explore my professional background as a Backend Engineer and discover my work and achievements in the field.',
-  ogDescription: 'Welcome to Mahmud Rafid\'s resume and portfolio site. Explore my professional background as a Backend Engineer and discover my work and achievements in the field.',
-  ogImage: 'https://pbs.twimg.com/profile_images/1694780129380380672/fGXGHRnK_400x400.jpg',
-  twitterCard: 'https://pbs.twimg.com/profile_images/1694780129380380672/fGXGHRnK_400x400.jpg',
+  meta: [
+    { name: 'description', content: 'Mahmud Rafid - Backend Engineer - Resume and Portfolio' },
+    { hid: 'og-type', property: 'og:type', content: 'website' },
+    { hid: 'og-title', property: 'og:title', content: 'Mahmud Rafid - Backend Engineer - Resume and Portfolio' },
+    { hid: 'og-desc', property: 'og:description', content: 'Welcome to Mahmud Rafid\'s resume and portfolio site. Explore my professional background as a Backend Engineer and discover my work and achievements in the field.' },
+    {
+      hid: 'og-image',
+      property: 'og:image',
+      content: 'https://pbs.twimg.com/profile_images/1694780129380380672/fGXGHRnK_400x400.jpg',
+    },
+    { hid: 'og-url', property: 'og:url', content: 'https://mahmud-rafid.vercel.app' },
+    { hid: 't-type', name: 'twitter:card', content: 'Mahmud Rafid | Backend Engineer | Resume' },
+  ],
+  // ogTitle: 'Mahmud Rafid - Backend Engineer - Resume and Portfolio',
+  // description: 'Welcome to Mahmud Rafid\'s resume and portfolio site. Explore my professional background as a Backend Engineer and discover my work and achievements in the field.',
+  // ogDescription: 'Welcome to Mahmud Rafid\'s resume and portfolio site. Explore my professional background as a Backend Engineer and discover my work and achievements in the field.',
+  // ogImage: 'https://pbs.twimg.com/profile_images/1694780129380380672/fGXGHRnK_400x400.jpg',
+  // twitterCard: 'https://pbs.twimg.com/profile_images/1694780129380380672/fGXGHRnK_400x400.jpg',
 })
 </script>
 
